f2f6581a414431badccbb8afa36ae40aaf6f20b1
brubsby <brubsbybrubsby@gmail.com>
2025-12-03 10:48:06 -0500

    Refactor: Extract CSS and JS to external files

--- js/db.js
+++ js/db.js
@@ -1,23 +1,23 @@
-const DB_NAME = 'RandomSiivaDB';
+const DB_NAME = "RandomSiivaDB";
 const DB_VERSION = 1;
-const STORE_NAME = 'vid_store';
+const STORE_NAME = "vid_store";
 
 const dbPromise = new Promise((resolve, reject) => {
   const request = indexedDB.open(DB_NAME, DB_VERSION);
 
-  request.onupgradeneeded = function(event) {
+  request.onupgradeneeded = function (event) {
     const db = event.target.result;
     if (!db.objectStoreNames.contains(STORE_NAME)) {
       db.createObjectStore(STORE_NAME);
     }
   };
 
-  request.onerror = function(event) {
+  request.onerror = function (event) {
     console.error("Database error: " + event.target.errorCode);
     reject(event.target.errorCode);
   };
 
-  request.onsuccess = function(event) {
+  request.onsuccess = function (event) {
     resolve(event.target.result);
   };
 });
@@ -26,7 +26,7 @@ const idbKeyval = {
   async get(key) {
     const db = await dbPromise;
     return new Promise((resolve, reject) => {
-      const transaction = db.transaction([STORE_NAME], 'readonly');
+      const transaction = db.transaction([STORE_NAME], "readonly");
       const store = transaction.objectStore(STORE_NAME);
       const request = store.get(key);
       request.onsuccess = () => resolve(request.result);
@@ -36,7 +36,7 @@ const idbKeyval = {
   async set(key, val) {
     const db = await dbPromise;
     return new Promise((resolve, reject) => {
-      const transaction = db.transaction([STORE_NAME], 'readwrite');
+      const transaction = db.transaction([STORE_NAME], "readwrite");
       const store = transaction.objectStore(STORE_NAME);
       const request = store.put(val, key);
       request.onsuccess = () => resolve();
@@ -46,7 +46,7 @@ const idbKeyval = {
   async delete(key) {
     const db = await dbPromise;
     return new Promise((resolve, reject) => {
-      const transaction = db.transaction([STORE_NAME], 'readwrite');
+      const transaction = db.transaction([STORE_NAME], "readwrite");
       const store = transaction.objectStore(STORE_NAME);
       const request = store.delete(key);
       request.onsuccess = () => resolve();
@@ -56,11 +56,11 @@ const idbKeyval = {
   async clear() {
     const db = await dbPromise;
     return new Promise((resolve, reject) => {
-      const transaction = db.transaction([STORE_NAME], 'readwrite');
+      const transaction = db.transaction([STORE_NAME], "readwrite");
       const store = transaction.objectStore(STORE_NAME);
       const request = store.clear();
       request.onsuccess = () => resolve();
       request.onerror = () => reject(request.error);
     });
-  }
+  },
 };
